26581d20e8e5856a4d5248810ddf2bfd91a76d78
alex <alex830216@gmail.com>
2021-10-17 16:07:30 +0800

    建立並顯示商品頁面

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,38 @@ class ProductsController < ApplicationController
   def index
   end
   
+  def show
+    @product = Product.find(params[:id])    
+  end
+  
   def new
   end
 
+  def edit
+
+  end
+
   def create
-    render plain: params[:product].inspect
+    # parmas 是一個 hash，用 [] 可以取得 hash 裡面屬性的值
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      
+    end
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :text)
   end
 
 end
